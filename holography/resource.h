56ec3b45f187f94e41b300a7cc197c3292f27de6
AlesMarso <alesxmaster1098@gmail.com>
2022-09-13 14:42:38 +0700

    Update res

--- holography/resource.h
+++ holography/resource.h
@@ -6,13 +6,14 @@
 #define ID_FILE                         40001
 #define ID_HOLOGRAM                     40002
 #define ID_ABOUT                        40003
+#define ID_BUTTON2                      40004
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40005
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
