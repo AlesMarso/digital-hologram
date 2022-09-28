e5a178e961fe6248ee2a0159fe3efbc2d58b25e3
AlesMarso <alesxmaster1098@gmail.com>
2022-09-28 11:28:44 +0700

    Chande liner system and main function

--- holography/src/main.cpp
+++ holography/src/main.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 #include <gui/Window.h>
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+int main()
 {
 	try
 	{
 		gui::Window window;
 
-		if (!window.Init(hInstance))
-			return -1;
+		if (!window.Init(GetModuleHandle(nullptr)))
+			throw std::exception("Failde window initialization");
+
+		std::cout << "Window initialization is complite" << std::endl;
 
 		if (!window.Create("Hologram"))
-			return -1;
+			throw std::exception("Failde window createion");
+
+		std::cout << "Window creation is complite" << std::endl;
 
 		int res = window.Run();
+
+		std::cout << "Window stop with code = " << res << std::endl;
 	}
 	catch (std::exception ex)
 	{
-
+		std::cout << ex.what() << std::endl;
 	}
 
 	return 0;
