9de00a2976b0e59ec2241b1e0a413377e4dc6fc0
AlesMarso <alesxmaster1098@gmail.com>
2022-10-27 07:56:59 +0700

    Добавил ипользование uniform переменной

--- holography/out/shaders/ComputeShader.glsl
+++ holography/out/shaders/ComputeShader.glsl
@@ -3,11 +3,13 @@
 layout(local_size_x = 1, local_size_y = 704, local_size_z = 1) in;
 layout(binding = 0, rgba8) uniform image2D out_tex;
 
+uniform uint PixelsX;
+
 void main()
 {
     uint texPosY = gl_GlobalInvocationID.y;
 
-	for(uint i = 0; i < 2816; i++)
+	for(uint i = 0; i < PixelsX; i++)
 	{
 		ivec2 texPos = ivec2(i, texPosY);     
 		float in_val = imageLoad( out_tex, texPos).r;
