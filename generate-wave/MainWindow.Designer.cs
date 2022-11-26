def97fb6fbc46d91f0d8652896830a314f835b4a
AlesPotapov1098 <awesome.ales1098@yandex.ru>
2022-11-26 12:47:54 +0700

    Пправки в графическом интерфейсе пользователя

--- generate-wave/MainWindow.Designer.cs
+++ generate-wave/MainWindow.Designer.cs
@@ -44,16 +44,21 @@ namespace generate_wave
             this.Chart = new System.Windows.Forms.PictureBox();
             this.saveImageDlg = new System.Windows.Forms.SaveFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.waveCharacteristics = new System.Windows.Forms.GroupBox();
+            this.waveTransport = new System.Windows.Forms.GroupBox();
+            this.fftWaveTransport = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.ResultImageWave)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Chart)).BeginInit();
+            this.waveCharacteristics.SuspendLayout();
+            this.waveTransport.SuspendLayout();
             this.SuspendLayout();
             // 
             // pointsCnt
             // 
-            this.pointsCnt.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.pointsCnt.Location = new System.Drawing.Point(506, 338);
+            this.pointsCnt.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pointsCnt.Location = new System.Drawing.Point(123, 117);
             this.pointsCnt.Name = "pointsCnt";
-            this.pointsCnt.Size = new System.Drawing.Size(61, 25);
+            this.pointsCnt.Size = new System.Drawing.Size(73, 23);
             this.pointsCnt.TabIndex = 31;
             this.pointsCnt.Text = "1024";
             this.pointsCnt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -62,17 +67,17 @@ namespace generate_wave
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.label2.Location = new System.Drawing.Point(407, 341);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(30, 120);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 19);
+            this.label2.Size = new System.Drawing.Size(80, 15);
             this.label2.TabIndex = 30;
             this.label2.Text = "Кол-во точек";
             // 
             // eraseImage
             // 
-            this.eraseImage.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.eraseImage.Location = new System.Drawing.Point(814, 336);
+            this.eraseImage.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.eraseImage.Location = new System.Drawing.Point(790, 338);
             this.eraseImage.Name = "eraseImage";
             this.eraseImage.Size = new System.Drawing.Size(85, 26);
             this.eraseImage.TabIndex = 29;
@@ -82,21 +87,21 @@ namespace generate_wave
             // 
             // createImage
             // 
-            this.createImage.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.createImage.Location = new System.Drawing.Point(698, 336);
+            this.createImage.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.createImage.Location = new System.Drawing.Point(659, 158);
             this.createImage.Name = "createImage";
-            this.createImage.Size = new System.Drawing.Size(85, 26);
+            this.createImage.Size = new System.Drawing.Size(33, 32);
             this.createImage.TabIndex = 28;
-            this.createImage.Text = "Создать";
+            this.createImage.Text = "->";
             this.createImage.UseVisualStyleBackColor = true;
             this.createImage.Click += new System.EventHandler(this.OnCreateImageClick);
             // 
             // saveImage
             // 
-            this.saveImage.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.saveImage.Location = new System.Drawing.Point(576, 336);
+            this.saveImage.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveImage.Location = new System.Drawing.Point(698, 338);
             this.saveImage.Name = "saveImage";
-            this.saveImage.Size = new System.Drawing.Size(85, 26);
+            this.saveImage.Size = new System.Drawing.Size(86, 26);
             this.saveImage.TabIndex = 27;
             this.saveImage.Text = "Сохранить";
             this.saveImage.UseVisualStyleBackColor = true;
@@ -106,7 +111,7 @@ namespace generate_wave
             // 
             this.ResultImageWave.BackColor = System.Drawing.Color.Silver;
             this.ResultImageWave.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.ResultImageWave.Location = new System.Drawing.Point(576, 9);
+            this.ResultImageWave.Location = new System.Drawing.Point(698, 9);
             this.ResultImageWave.Name = "ResultImageWave";
             this.ResultImageWave.Size = new System.Drawing.Size(323, 323);
             this.ResultImageWave.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -115,10 +120,10 @@ namespace generate_wave
             // 
             // wavesCnt
             // 
-            this.wavesCnt.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.wavesCnt.Location = new System.Drawing.Point(364, 338);
+            this.wavesCnt.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.wavesCnt.Location = new System.Drawing.Point(123, 59);
             this.wavesCnt.Name = "wavesCnt";
-            this.wavesCnt.Size = new System.Drawing.Size(37, 25);
+            this.wavesCnt.Size = new System.Drawing.Size(73, 23);
             this.wavesCnt.TabIndex = 25;
             this.wavesCnt.Text = "1";
             this.wavesCnt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -127,19 +132,19 @@ namespace generate_wave
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.label4.Location = new System.Drawing.Point(271, 341);
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(30, 62);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(87, 19);
+            this.label4.Size = new System.Drawing.Size(76, 15);
             this.label4.TabIndex = 24;
             this.label4.Text = "Кол-во волн";
             // 
             // phase
             // 
-            this.phase.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.phase.Location = new System.Drawing.Point(217, 338);
+            this.phase.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.phase.Location = new System.Drawing.Point(123, 88);
             this.phase.Name = "phase";
-            this.phase.Size = new System.Drawing.Size(48, 25);
+            this.phase.Size = new System.Drawing.Size(73, 23);
             this.phase.TabIndex = 23;
             this.phase.Text = "0,0";
             this.phase.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -148,33 +153,33 @@ namespace generate_wave
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.label3.Location = new System.Drawing.Point(172, 341);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(30, 91);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(39, 19);
+            this.label3.Size = new System.Drawing.Size(33, 15);
             this.label3.TabIndex = 22;
             this.label3.Text = "Фаза";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.label1.Location = new System.Drawing.Point(13, 341);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 19);
+            this.label1.Size = new System.Drawing.Size(66, 15);
             this.label1.TabIndex = 21;
             this.label1.Text = "Вид волны";
             // 
             // waveType
             // 
-            this.waveType.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.waveType.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.waveType.FormattingEnabled = true;
             this.waveType.Items.AddRange(new object[] {
             "sin(x)",
             "sin(x)/x"});
-            this.waveType.Location = new System.Drawing.Point(96, 338);
+            this.waveType.Location = new System.Drawing.Point(84, 30);
             this.waveType.Name = "waveType";
-            this.waveType.Size = new System.Drawing.Size(70, 25);
+            this.waveType.Size = new System.Drawing.Size(112, 23);
             this.waveType.TabIndex = 20;
             this.waveType.SelectedIndexChanged += new System.EventHandler(this.OnWaveTypeSelectedIndexChanged);
             // 
@@ -185,36 +190,68 @@ namespace generate_wave
             this.Chart.Location = new System.Drawing.Point(9, 9);
             this.Chart.Margin = new System.Windows.Forms.Padding(0);
             this.Chart.Name = "Chart";
-            this.Chart.Size = new System.Drawing.Size(558, 323);
+            this.Chart.Size = new System.Drawing.Size(636, 323);
             this.Chart.TabIndex = 19;
             this.Chart.TabStop = false;
             this.Chart.Click += new System.EventHandler(this.Chart_Click);
             this.Chart.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
             // 
+            // waveCharacteristics
+            // 
+            this.waveCharacteristics.Controls.Add(this.waveType);
+            this.waveCharacteristics.Controls.Add(this.label2);
+            this.waveCharacteristics.Controls.Add(this.pointsCnt);
+            this.waveCharacteristics.Controls.Add(this.label1);
+            this.waveCharacteristics.Controls.Add(this.wavesCnt);
+            this.waveCharacteristics.Controls.Add(this.label4);
+            this.waveCharacteristics.Controls.Add(this.phase);
+            this.waveCharacteristics.Controls.Add(this.label3);
+            this.waveCharacteristics.Location = new System.Drawing.Point(9, 346);
+            this.waveCharacteristics.Name = "waveCharacteristics";
+            this.waveCharacteristics.Size = new System.Drawing.Size(208, 147);
+            this.waveCharacteristics.TabIndex = 32;
+            this.waveCharacteristics.TabStop = false;
+            this.waveCharacteristics.Text = "Характерстики волны";
+            // 
+            // waveTransport
+            // 
+            this.waveTransport.Controls.Add(this.fftWaveTransport);
+            this.waveTransport.Location = new System.Drawing.Point(224, 346);
+            this.waveTransport.Name = "waveTransport";
+            this.waveTransport.Size = new System.Drawing.Size(421, 147);
+            this.waveTransport.TabIndex = 33;
+            this.waveTransport.TabStop = false;
+            this.waveTransport.Text = "Преобразования";
+            // 
+            // fftWaveTransport
+            // 
+            this.fftWaveTransport.Location = new System.Drawing.Point(6, 29);
+            this.fftWaveTransport.Name = "fftWaveTransport";
+            this.fftWaveTransport.Size = new System.Drawing.Size(56, 23);
+            this.fftWaveTransport.TabIndex = 0;
+            this.fftWaveTransport.Text = "БПФ";
+            this.fftWaveTransport.UseVisualStyleBackColor = true;
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(909, 370);
-            this.Controls.Add(this.pointsCnt);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(1033, 504);
+            this.Controls.Add(this.waveTransport);
+            this.Controls.Add(this.waveCharacteristics);
             this.Controls.Add(this.eraseImage);
             this.Controls.Add(this.createImage);
             this.Controls.Add(this.saveImage);
             this.Controls.Add(this.ResultImageWave);
-            this.Controls.Add(this.wavesCnt);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.phase);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.waveType);
             this.Controls.Add(this.Chart);
             this.Name = "MainWindow";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.ResultImageWave)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Chart)).EndInit();
+            this.waveCharacteristics.ResumeLayout(false);
+            this.waveCharacteristics.PerformLayout();
+            this.waveTransport.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -235,6 +272,9 @@ namespace generate_wave
         private System.Windows.Forms.PictureBox Chart;
         private System.Windows.Forms.SaveFileDialog saveImageDlg;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.GroupBox waveCharacteristics;
+        private System.Windows.Forms.GroupBox waveTransport;
+        private System.Windows.Forms.Button fftWaveTransport;
     }
 }
 
